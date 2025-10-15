5857083f7741228a5a5aef7fb6f17f50c0003cc7
Yury <not.iron.yuriy@inbox.ru>
2025-10-15 20:06:46 +0300

    feat(verification): Добавлен и подключен модуль VerificationModule для верификации пользователей

--- src/modules/auth/auth.module.ts
+++ src/modules/auth/auth.module.ts
@@ -4,10 +4,12 @@ import { UsersModule } from 'src/modules/users/users.module';
 import { UserRepository } from 'src/modules/users/users.repository';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
+import { VerificationModule } from 'src/modules/verification/verification.module';
+import { VerificationService } from 'src/modules/verification/verification.service';
 
 @Module({
-  imports: [UsersModule],
+  imports: [UsersModule, VerificationModule],
   controllers: [AuthController],
-  providers: [AuthService, UserRepository, PrismaService],
+  providers: [AuthService, UserRepository, PrismaService, VerificationService],
 })
 export class AuthModule {}
--- src/modules/verification/verification.module.ts
+++ src/modules/verification/verification.module.ts
@@ -1,10 +1,22 @@
 import { Module } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { PinoLogger } from 'pino-nestjs';
+import { PrismaService } from 'src/common/services/prisma.service';
+import { UserRepository } from 'src/modules/users/users.repository';
 import { VerificationController } from './verification.controller';
 import { VerificationRepository } from './verification.repository';
 import { VerificationService } from './verification.service';
 
 @Module({
   controllers: [VerificationController],
-  providers: [VerificationService, VerificationRepository],
+  providers: [
+    VerificationService,
+    VerificationRepository,
+    UserRepository,
+    PrismaService,
+    ConfigService,
+    PinoLogger,
+  ],
+  exports: [VerificationService, VerificationRepository],
 })
 export class VerificationModule {}
